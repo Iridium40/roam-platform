6b1edb342c07df5a2d5eb94eb91b6b559f2f5cf6
Iridium40 <alan@roamyourbestlife.com>
2025-08-25 11:16:22 -0500

    Try Option 2: Switch from @vitejs/plugin-react-swc to @vitejs/plugin-react for all apps

--- roam-provider-app/vite.config.ts
+++ roam-provider-app/vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
-    },
-  },
   build: {
     outDir: "dist/spa",
-  },
-  server: {
-    port: 8080,
-    host: true,
   },
 });
 
