50514cd3ee35287eac8a327422a99f4549de5885
Iridium40 <alan@roamyourbestlife.com>
2025-08-25 11:09:41 -0500

    Simplify main.tsx files: Remove dynamic root element creation for all apps

--- roam-provider-app/main.tsx
+++ roam-provider-app/main.tsx
@@ -2,15 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './client/App.tsx'
 
-// Create root element if it doesn't exist
-const rootElement = document.getElementById('root') || (() => {
-  const div = document.createElement('div')
-  div.id = 'root'
-  document.body.appendChild(div)
-  return div
-})()
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
